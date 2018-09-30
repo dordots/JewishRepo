69819b3a1266cbeb76f9c6feb17e3db33d9928b6
Moshe Rimok <mosherk16@gmail.com>
2018-09-30 14:07:41 +0300

    referred to maxRadiusRange

--- src/common/models/event/search-event.ts
+++ src/common/models/event/search-event.ts
@@ -1,9 +1,11 @@
 import {CreateSynagogueOptions, SynagogueOptions} from "../common/enums/synagogue-option";
 import {MapObject} from "../map-objects/map-objects";
+import moment = require("moment");
 
 export class SearchEvent {
   name: string;
-  radiusRange: number;
+  radiusRange = 1;
+  maxRadiusRange = 25;
   mapObject: MapObject;
   startTime: Date;
   endTime: Date;
@@ -16,5 +18,21 @@ export class SearchEvent {
     this.prayerNosach = [];
     this.mapObject = new MapObject();
     this.synagogueOptions = CreateSynagogueOptions();
+  }
+
+  toServerModel(){
+    return {
+      name: this.name,
+      address: this.mapObject.userFriendlyAddress,
+      lat: this.mapObject.latLng.lat,
+      lon: this.mapObject.latLng.lng,
+      min_radius: this.radiusRange,
+      max_radius: this.maxRadiusRange,
+      startTime: this.startTime && this.startTime && moment(this.startTime).format('hh:mm'),
+      endTime: this.endTime && this.endTime && moment(this.endTime).format('hh:mm'),
+      days: this.daysRange,
+      externals: this.synagogueOptions,
+      nosach: this.prayerNosach
+    };
   }
 }
