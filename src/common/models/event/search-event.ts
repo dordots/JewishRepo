bbb4885bd321c15f999950ada144a84443e07f5e
Moshe Rimok <mosherk16@gmail.com>
2018-09-30 14:25:01 +0300

    Omri wanted the synagogueOptions (externals) will be sent in particular format

--- src/common/models/event/search-event.ts
+++ src/common/models/event/search-event.ts
@@ -1,6 +1,7 @@
 import {CreateSynagogueOptions, SynagogueOptions} from "../common/enums/synagogue-option";
 import {MapObject} from "../map-objects/map-objects";
 import moment = require("moment");
+import {keys} from "lodash-es";
 
 export class SearchEvent {
   name: string;
@@ -21,18 +22,23 @@ export class SearchEvent {
   }
 
   toServerModel(){
-    return {
+    const model = {
       name: this.name,
       address: this.mapObject.userFriendlyAddress,
       lat: this.mapObject.latLng.lat,
       lon: this.mapObject.latLng.lng,
       min_radius: this.radiusRange,
       max_radius: this.maxRadiusRange,
-      startTime: this.startTime && this.startTime && moment(this.startTime).format('hh:mm'),
-      endTime: this.endTime && this.endTime && moment(this.endTime).format('hh:mm'),
+      start_time: this.startTime && this.startTime && moment(this.startTime).format('hh:mm'),
+      end_time: this.endTime && this.endTime && moment(this.endTime).format('hh:mm'),
       days: this.daysRange,
-      externals: this.synagogueOptions,
       nosach: this.prayerNosach
     };
+    keys(this.synagogueOptions).forEach(k => {
+      if (this.synagogueOptions[k] && this.synagogueOptions[k] == true)
+        model[`externals.${k}`] = true;
+    });
+
+    return model;
   }
 }
