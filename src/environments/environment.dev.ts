254be27c0165dbb0dfcc597773322e8356c26b27
Vladi Rachlin <vladi.r95@gmail.com>
2019-01-04 20:41:02 +0200

    minor

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const Config = {
-  serverBaseUrl: "httaps://jewish-71aed.appspot.com/",
+  serverBaseUrl: "https://jewish-71aed.appspot.com/",
   iconsBasePath: "assets/icons",
   watchLocationIntervalInMs: 1000,
   locateMeTimeoutInMs: 3000
